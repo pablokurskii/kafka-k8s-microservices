e7b7de9ed53a59648e0c636608fb905d72f10ad7
Pablo Kurskii <pablo.kurskii@gmail.com>
2022-03-26 23:16:06 +0100

    Spring cloud gateway
    API Gateway - Spring cloud gateway - to handle requests, to keep all services in private network so clients never talk directly to the microservices, sometimes instead of this starter Netflix Zuul used.

--- apigw/src/main/java/org/example/apigw/ApiGWApplication.java
+++ apigw/src/main/java/org/example/apigw/ApiGWApplication.java
@@ -1,17 +1,13 @@
-package org.example.customer;
+package org.example.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableFeignClients(
-        basePackages = "org.example.clients"
-)
-public class CustomerApplication {
+public class ApiGWApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CustomerApplication.class, args);
+        SpringApplication.run(ApiGWApplication.class, args);
     }
 }
