13b5d2d392c7f85e4e80a63471d29ca00f1217fc
Pablo Kurskii <pablo.kurskii@gmail.com>
2022-03-26 22:07:31 +0100

    Sleuth + Zipkin  request tracing
     * Sleuth + SL4j request tracing
     * Zipkin - distributed tracing system that helps gather sleuth trace log + analysis
    In the production usually stores in Elasticsearch + Kibana + Logstash stack

--- HELP.md
+++ HELP.md
@@ -1,2 +1,6 @@
 - run docker compose from terminal
 docker compose up -d
+
+http://localhost:5050/browser/ - pgadmin
+http://localhost:8761/ - eureka
+http://localhost:9411/zipkin/ -zipkin
--- fraud/src/main/java/org/example/fraud/controller/FraudController.java
+++ fraud/src/main/java/org/example/fraud/controller/FraudController.java
@@ -1,6 +1,7 @@
 package org.example.fraud.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.clients.fraud.FraudCheckResponse;
 import org.example.fraud.service.FraudCheckService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
     private final FraudCheckService fraudCheckService;
 
@@ -18,6 +20,7 @@ public class FraudController {
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService
                 .isFraudulentCustomer(customerId);
+        log.info("customerId {} is fraud: {}", customerId, isFraudulentCustomer);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
