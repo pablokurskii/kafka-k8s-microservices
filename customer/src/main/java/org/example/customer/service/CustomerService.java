a36e46048f1b8ec4a7c74575ef91bdc05157457c
Pablo Kurskii <pablo.kurskii@gmail.com>
2022-03-26 12:59:31 +0100

    Service discovery + LoadBalancer

--- customer/src/main/java/org/example/customer/service/CustomerService.java
+++ customer/src/main/java/org/example/customer/service/CustomerService.java
@@ -24,7 +24,7 @@ public class CustomerService {
 //        TODO check email not taken
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
